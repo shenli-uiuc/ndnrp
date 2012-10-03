44150bc70d498863d93249d112df634e48599b87
Geni <geni@ubuntu.ubuntu-domain>
2012-10-03 18:44:48 -0500

    continue

--- src/ipsrc/client/IPClient.java
+++ src/ipsrc/client/IPClient.java
@@ -10,28 +10,107 @@ import java.util.*;
 public class IPClient{
     public static final int PUBLISHER = 1 << 0;
     public static final int SUBSCRIBER = 1 << 1;
+    public static final int BUF_LEN = 1024;
 
     private String _name = null;
     private HashSet<String> _subSet = null;
     private Socket _socket = null;
 
+    private byte[] _buf = null;
+
     public IPClient(String name, int type){
         this._name = name;
         this._subSet = new HashSet<String>();
+        this._buf = new byte[BUF_LEN];
         if(type | SUBSCRIBER){
-            _socket = new Socket(Protocol.SERVER_IP, Protocol.SERVER_PORT);
+            this._socket = getSocket();
         }
     }    
 
+    private Socket getSocket(){
+        try{
+            if(null != _socket && !_socket.isClosed()){
+                return _socket;
+            }
+            else{
+                return new Socket(Protocol.SERVER_IP, Protocol.SERVER_PORT);
+            }
+        }
+        catch(UnknownHostException ex){
+            ex.printStackTrace();
+        }
+        catch(IOException ex){
+            ex.printStackTrace();
+        }
+        catch()
+    }
 
-    public synchronized boolean subscribe(String pubName){
-        return true;
+    public void subscribe(String pubName){
+        if(null == _socket){
+            _socket= getSocket();
+        }
+        String subMsg = Protocol.HEAVY_SUB_PREFIX + _name + "/" + pubName;
+        send(_socket, subMsg);
+    }
+
+
+    public void unsubscribe(String pubName){
+        if(null == _socket){    
+            _socket = getSocket();
+        }
+        String unsMsg = Protocol.HEAVY_UNSUB_PREFIX + _name + "/" + pubName;
+        send(_socket, unsMsg);
+    }
+
+    private void send(Socket socket, String msg){
+        if(null == socket || socket.isClosed()){
+            System.out.println("Error in IPClient.send: socket is null");
+            return;
+        }
+        try{
+            OutputStream out = socket.getOutputStream();
+            out.write(msg.getBytes(Protocol.ENCODING));
+            out.flush(); 
+        }
+        catch(UnsupportedEncodingException ex){
+            ex.printStackTrace();
+        }
+        catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void listen(){
+        if(null == _socket || _socket.isClosed()){
+            _socket = getSocket();
+        }
+        String listenMsg = Protocol.HEAVY_LISTEN_PREFIX + _name;
+        send(_socket, listenMsg);
     }
 
     public String receive(){
+        try{
+            if(null == _socket || _socket.isClosed()){
+                _socket = getSocket();
+            }
+            InputStream in = _socket.getInputStream();
+            
+        }
+        catch(IOException ex){
+            ex.printStackTrace();
+        }
         return null;
     }
 
     public void post(String msg){
+        Socket socket = getSocket();
+
+        String postMsg = Protocol.HEAVY_POST_PREFIX + _name + "/" + msg;
+        send(socket, postMsg);
+        if(socket != _socket){
+            out.close();
+            socket.close();
+            socket = null;
+        }
     }
 }
--- src/protocol/Protocol.java
+++ src/protocol/Protocol.java
@@ -3,6 +3,7 @@ package ndnrp.protocol;
 public class Protocol{
     public static final String SERVER_IP = "127.0.0.1";
     public static final int SERVER_PORT = 20480;
+    public static final String ENCODING = "UTF-16LE";
 
     public static final String LIGHT_POST_PREFIX = "ccnx:/ndnrp/light/post/";
     public static final String LIGHT_PUB_PREFIX = "ccnx:/ndnrp/light/pub/";
